75d0ae88d1a52aabfdf27402f36dcc2f09cf91e1
jordannakamoto <jordannakamoto@gmail.com>
2024-02-13 18:19:50 -0800

    wrote comment block

--- JordanVersion/include/CommentIdentifierDFA.h
+++ JordanVersion/include/CommentIdentifierDFA.h
@@ -1,4 +1,21 @@
 // CommentIdentifierDFA.h
+
+/**
+ * The CommentIdentifierDFA class implements a deterministic finite automaton (DFA) 
+ * to identify comments within a stream of characters. It is designed to support 
+ * both block comments (delimited by "/*" and "(*)/") and line comments (delimited by "//" and "\n").
+ * 
+ * Key Features:
+ *  - Employs a procedural state machine to transition between states based on the current character in the stream.
+ * 
+ * Usage Conditions:
+ *  - Initialize the DFA, which sets the state to START.
+ *  - For each character in the input stream, call processChar(char ch) to process the character through the DFA.
+ *  - At any point, isActive() can be queried to determine if the DFA is within a comment or an potential initial delimiter '/'+...
+ * 
+ * This class is designed for preprocessing C/C++ files.
+ */
+
 #ifndef COMMENT_IDENTIFIER_DFA_H
 #define COMMENT_IDENTIFIER_DFA_H
 
