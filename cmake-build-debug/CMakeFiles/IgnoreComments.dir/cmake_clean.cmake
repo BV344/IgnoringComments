cc2dc6715b108d929c9573e421d07d207ae1f99d
Brayan Vizcaino <brayan.vizcaino@gmail.com>
2024-02-13 18:34:22 -0800

    I had to change the version of cmake to 3.24

---
+++ cmake-build-debug/CMakeFiles/IgnoreComments.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IgnoreComments.dir/Tokenizer.cpp.obj"
+  "CMakeFiles/IgnoreComments.dir/Tokenizer.cpp.obj.d"
+  "CMakeFiles/IgnoreComments.dir/main.cpp.obj"
+  "CMakeFiles/IgnoreComments.dir/main.cpp.obj.d"
+  "IgnoreComments.exe"
+  "IgnoreComments.exe.manifest"
+  "IgnoreComments.pdb"
+  "libIgnoreComments.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IgnoreComments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
