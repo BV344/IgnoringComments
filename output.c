d801082b947f6ebf6f3e89b59d6bd6b73c118c6d
Brayan Vizcaino <brayan.vizcaino@gmail.com>
2024-02-13 19:58:11 -0800

    testing output2

---
+++ output.c
@@ -0,0 +1,16 @@
+                                                       
+                                                       
+                                                       
+                                 
+procedure main (         void)
+{
+  int                    counter; 
+
+  counter =       101;
+                     
+  printf ("counter = %d\n", counter);
+  printf ("/* Will this string be displayed? */\n");
+   
+  
+  
+}
